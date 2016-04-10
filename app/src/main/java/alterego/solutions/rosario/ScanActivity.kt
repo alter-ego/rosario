4e991053e525912cec1a4fdde0e19ad8e54239e0
Ivan Morgillo <imorgillo@gmail.com>
2016-04-10 09:24:44 +0200

    Add Udoo connect/read PoF

--- app/src/main/java/alterego/solutions/rosario/ScanActivity.kt
+++ app/src/main/java/alterego/solutions/rosario/ScanActivity.kt
@@ -1,14 +1,15 @@
 package alterego.solutions.rosario
 
+import android.bluetooth.le.ScanResult
 import android.os.Bundle
 import android.os.Handler
 import android.support.v7.app.AppCompatActivity
+import org.udoo.udooblulib.interfaces.IBleDeviceListener
 import org.udoo.udooblulib.manager.UdooBluManager
+import org.udoo.udooblulib.scan.BluScanCallBack
 import org.udoo.udooblulib.sensor.Constant
+import org.udoo.udooblulib.sensor.UDOOBLESensor
 import rx.Subscription
-import rx.android.schedulers.AndroidSchedulers
-import rx.schedulers.Schedulers
-import timber.log.Timber
 import javax.inject.Inject
 
 class ScanActivity : AppCompatActivity() {
@@ -25,24 +26,47 @@ class ScanActivity : AppCompatActivity() {
         App[this].component().inject(this);
         setContentView(R.layout.activity_scan)
 
-        Handler().postDelayed(
-            {
-                udooBluManager.scanLeDevice(true)
-                    .filter({ result -> result.toString().contains("B0:B4:48:") })
-                    .distinctUntilChanged({ it.device })
-                    .doOnNext { address = it.device.address }
-                    .concatMap { udooBluManager.connect(it.device.address) }
-                    .filter({ true })
-                    .map { udooBluManager.discoveryServices(address); }
-                    .filter({ true })
-                    .map { udooBluManager.setIoPinMode(address, Constant.IOPIN.D6, Constant.IOPIN_TYPE.DIGITAL, Constant.IOPIN_MODE.INPUT) }
-                    .subscribeOn(Schedulers.io())
-                    .observeOn(AndroidSchedulers.mainThread())
-                    .subscribe(
-                        {
-                            Timber.d(udooBluManager.digitalRead(address, Constant.IOPIN.D6)[0].toInt().toString())
-                        },
-                        { error -> Timber.e(error.message) })
-            }, 4000)
+        Handler().postDelayed({
+            udooBluManager.scanLeDevice(true, object : BluScanCallBack() {
+                override fun onBatchScanResults(results: MutableList<ScanResult>?) {
+                    super.onBatchScanResults(results)
+                }
+
+                override fun onScanFailed(errorCode: Int) {
+                    super.onScanFailed(errorCode)
+                }
+
+                override fun onScanResult(callbackType: Int, result: ScanResult?) {
+                    super.onScanResult(callbackType, result)
+                    val device = result?.device
+                    if (device?.address!!.startsWith("B0:B4:48")) {
+                        udooBluManager.connect(device?.address, object : IBleDeviceListener {
+                            override fun onDeviceConnected() {
+                                udooBluManager.discoveryServices(device?.address)
+                            }
+
+                            override fun onDeviceDisconnect() {
+                            }
+
+                            override fun onServicesDiscoveryCompleted() {
+                                udooBluManager.enableSensor(device?.address, UDOOBLESensor.IOPIN, true)
+                                udooBluManager.setIoPinMode(device?.address, Constant.IOPIN.D6, Constant.IOPIN_TYPE.DIGITAL, Constant.IOPIN_MODE.INPUT)
+                                while (true) {
+                                    udooBluManager.digitalRead(device?.address, Constant.IOPIN.D6)
+                                    Thread.sleep(200)
+                                }
+                            }
+                        })
+                    }
+
+                }
+            });
+
+        }, 100);
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        scanSubscription.unsubscribe()
     }
 }
